a1f3ca728dddafcb8598765b40ff11b89f41981b
Yangshun Tay <tay.yang.shun@gmail.com>
2019-05-31 20:23:52 -0700

    Revert image

--- website/theme/components/Image.js
+++ website/theme/components/Image.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import withBaseUrl from '@docusaurus/withBaseUrl';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 function Image(props) {
-  return <img {...props} src={withBaseUrl(props.src)} />;
+  return <img {...props} src={props.src} />;
+}
+
+function StaticImage(props) {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  const { baseUrl } = siteConfig;
+  const src = baseUrl + props.src;
+  return <Image {...props} src={src} />;
 }
 
 export default Image;
